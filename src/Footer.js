d8a978ba137883f198530fc6ddf3276392c48de5
park ye jin <dyddyd0514@naver.com>
2021-06-27 18:44:05 +0900

    Feat Footer

---
+++ src/Footer.js
@@ -0,0 +1,109 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Footer() {
+  return (
+    <Container>
+      <LogoImage src={process.env.PUBLIC_URL + "redBandanaLogo.svg"} />
+
+      <Text>
+        자주 묻는 질문
+        <br />
+        이용약관
+        <br />
+        개인정보처리방안
+      </Text>
+
+      <SubContainer>
+        <SubTextType>
+          상호명
+          <br />
+          이메일
+          <br />
+          대표
+          <br />
+          연락처
+        </SubTextType>
+
+        <SubTextValue>
+          Red Bandana
+          <br />
+          redbandana@gmail.com
+          <br />
+          Bandana Kim
+          <br />
+          02-1234-5678
+        </SubTextValue>
+      </SubContainer>
+      <IconContainer>
+        <img src={process.env.PUBLIC_URL + "InstagramLogo.svg"} />
+        <img src={process.env.PUBLIC_URL + "KakaoTalkLogo.svg"} />
+      </IconContainer>
+
+      <CompanyText>2021 Redbandana Corp. All Rights Reserved</CompanyText>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #61656a;
+  height: 565px;
+  padding: 0px 16px;
+`;
+const LogoImage = styled.img`
+  margin: 61px 0px 0px 0px;
+  width: 206px;
+  height: 14px;
+  object-fit: cover;
+`;
+
+const Text = styled.div`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 13px;
+  line-height: 30px;
+  color: #ffffff;
+  margin: 0px;
+  margin-top: 19px;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 19px;
+`;
+
+const SubTextType = styled.div`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 13px;
+  line-height: 22px;
+  color: rgba(255, 255, 255, 0.6);
+`;
+
+const SubTextValue = styled(SubTextType)`
+  font-weight: normal;
+  margin-left: 32.25px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 112px;
+  margin-top: 82.76px;
+`;
+
+const CompanyText = styled.p`
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 35px;
+  text-align: center;
+  color: #ffffff;
+  margin: 10px 0px;
+`;
